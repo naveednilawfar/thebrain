6543a36e8259e504fbe89d5ab9435bc6ba010439
naveednilawfar <naveednilawfar@gmail.com>
2022-03-13 16:14:17 +1100

    Initialize pypi project

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="brain",
+    name="thebrain",
     version="1.0",
     author="Naveed Nilawfar",
     author_email="naveednilawfar@gmail.com",
     description="The human brain implemented in python (AI).",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/naveednilawfar/brain",
+    url="https://github.com/naveednilawfar/thebrain",
     project_urls={
-        "Bug Tracker": "https://github.com/naveednilawfar/brain/issues",
+        "Bug Tracker": "https://github.com/naveednilawfar/thebrain/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
